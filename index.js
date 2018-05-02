1eb87c1498e52341c589e426bbdb89556e9f9a80
Tyler Dreher <drehertyler@gmail.com>
2018-05-02 20:07:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ function deepestChild() {
 function increaseRankBy(n) {
   var all = document.querySelectorAll('.ranked-list');
   var rList = [];
-  for (let i = 0; i < all.length; i++)
+  for (let i = 0; i < all.length; i++) {
     rList = all[0].children;
     console.log(rList);
     for (let ii = 0; ii < rList.length; ii++) {
       rList[ii].innerHTML = parseInt(rList[ii].innerHTML) + n;
     }
     console.log(i);
+  }
 }
