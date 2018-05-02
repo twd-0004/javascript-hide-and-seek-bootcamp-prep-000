482fe35459f594e477771b9729de1f6512f18b67
Tyler Dreher <drehertyler@gmail.com>
2018-05-02 20:41:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,19 +31,19 @@ function deepestChild() {
   return current;
 }
 
-function find(array, criteriaFn) {
-  let current = array
-  let next = []
-  while (current) {
-    if (criteriaFn(current)) {
-      return current
-    }
-    if (Array.isArray(current)) {
-      for (let i = 0; i < current.length; i++) {
-        next.push(current[i])
-      }
-    }
-    current = next.shift()
-  }
-  return null
-}
+// function find(array, criteriaFn) {
+//   let current = array
+//   let next = []
+//   while (current) {
+//     if (criteriaFn(current)) {
+//       return current
+//     }
+//     if (Array.isArray(current)) {
+//       for (let i = 0; i < current.length; i++) {
+//         next.push(current[i])
+//       }
+//     }
+//     current = next.shift()
+//   }
+//   return null
+// }
