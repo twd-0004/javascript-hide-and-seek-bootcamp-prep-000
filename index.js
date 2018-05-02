bed868289eaad9e6806f0c848fc4e895ec87482f
Tyler Dreher <drehertyler@gmail.com>
2018-05-02 20:08:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
   var all = document.querySelectorAll('.ranked-list');
   var rList = [];
   for (let i = 0; i < all.length; i++) {
-    rList = all[0].children;
+    rList = all[i].children;
     console.log(rList);
     for (let ii = 0; ii < rList.length; ii++) {
       rList[ii].innerHTML = parseInt(rList[ii].innerHTML) + n;
