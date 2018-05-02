a399d7a2c22b7295f19dbd4232f3695d078f8b62
Tyler Dreher <drehertyler@gmail.com>
2018-05-02 19:48:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return getFirstSelector('#nested').querySelector('.target')
 }
+
+function deepestChild() {
+
+}
+
+function increaseRankBy(n) {
+  var rList = getFirstSelector('.ranked-list').children
+  for (let i = 0; i < rList.length; i++) {
+    rList[i].innerHTML = parseInt(rList[i].innerHTML) + n;
+  }
+}
